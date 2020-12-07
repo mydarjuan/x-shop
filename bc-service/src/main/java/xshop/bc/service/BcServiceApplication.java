9f7a24e9cec9d1465f645db4a98c11212e2bca68
liujianbo <350235397@qq.com>
2020-12-07 16:30:19 +0800

    研究consul 服务注册发现

---
+++ bc-service/src/main/java/xshop/bc/service/BcServiceApplication.java
@@ -0,0 +1,17 @@
+package xshop.bc.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication()
+@EnableFeignClients
+@EnableDiscoveryClient
+public class BcServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BcServiceApplication.class, args);
+    }
+
+}
---
+++ bc-service/src/test/java/xshop/bc/service/BcServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package xshop.bc.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
